a0b5ccac61ff5fe6ccf25ccf281b8b3286cfefef
alexaldearroyo <alexaldearroyo@gmail.com>
2025-04-25 23:25:23 +0200

    fix bg colors

--- core/styles.go
+++ core/styles.go
@@ -6,11 +6,11 @@ import "github.com/charmbracelet/lipgloss"
 // Colors
 var (
 	// Directories
-	Green = lipgloss.NewStyle().Foreground(lipgloss.Color("2")).Background(lipgloss.Color("0"))
+	Green = lipgloss.NewStyle().Foreground(lipgloss.Color("2"))
 	// Panel Headers
-	Cyan = lipgloss.NewStyle().Foreground(lipgloss.Color("6")).Background(lipgloss.Color("0"))
+	Cyan = lipgloss.NewStyle().Foreground(lipgloss.Color("6"))
 	// Files
-	White = lipgloss.NewStyle().Foreground(lipgloss.Color("7")).Background(lipgloss.Color("0"))
+	White = lipgloss.NewStyle().Foreground(lipgloss.Color("7"))
 	// Selected item
 	// Marked items
 	Marked = lipgloss.NewStyle().Foreground(lipgloss.Color("11")).Background(lipgloss.Color("0"))
@@ -33,10 +33,10 @@ var (
 // Styles for the layout (combined into one block)
 var (
 	// Directory text and the directory display
-	DirectoryText = lipgloss.NewStyle().Foreground(lipgloss.Color("7")).Background(lipgloss.Color("0"))
-	DirectoryDir  = lipgloss.NewStyle().Foreground(lipgloss.Color("3")).Background(lipgloss.Color("0"))
+	DirectoryText = lipgloss.NewStyle().Foreground(lipgloss.Color("7"))
+	DirectoryDir  = lipgloss.NewStyle().Foreground(lipgloss.Color("3"))
 	// Title on the right (Cat Explorer)
-	HeaderTitle = lipgloss.NewStyle().Foreground(lipgloss.Color("3")).Background(lipgloss.Color("0"))
+	HeaderTitle = lipgloss.NewStyle().Foreground(lipgloss.Color("3"))
 )
 
 
