5a1d807cc9f9ddba904c9e4879016bf0622f4bc6
alexaldearroyo <alexaldearroyo@gmail.com>
2025-04-26 08:28:49 +0200

    add dviser

--- core/draw.go
+++ core/draw.go
@@ -177,7 +177,7 @@ func DrawLayout(position int, items []string, currentDir string, files []string,
 	// Left panel (Directories)
 	selected := map[string]bool{}
 	start := 0
-	panelHeight := height - 5  // Adjusted to consider the additional line of the status bar
+	panelHeight := height - 7  // Adjusted to consider the additional line of the status bar
 	active := activePanel == 1
 	includeSubdirs := false
 
